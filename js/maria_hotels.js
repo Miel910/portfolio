4d7c12aed6a06b08f7f9aa107b0cd47a54bf57de
Miel910 <111350781+Miel910@users.noreply.github.com>
2023-03-19 21:53:39 +0300

    Add files via upload

---
+++ js/maria_hotels.js
@@ -0,0 +1,14 @@
+/* Abulmohsen Alfreh */
+/* ID: 439010208 */
+
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+//     document.getElementById("navbar").style.padding = "5px 10px";
+//     document.getElementById("logo").style.width = "50px";
+//   } else {
+//     document.getElementById("navbar").style.padding = "5px 10px";
+//     document.getElementById("logo").style.width = "70px";
+//   }
+// }
